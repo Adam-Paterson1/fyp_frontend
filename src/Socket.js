beb392f1f29408c54386af19edbc317a473450a3
Adam-Paterson1 <adam.paterson03@gmail.com>
2019-07-05 09:30:29 +0000

    Manage state better

--- src/Socket.js
+++ src/Socket.js
@@ -1,38 +1,20 @@
-// // Create WebSocket connection.
-// const socket = new WebSocket('ws://localhost:8080');
-
-// // Connection opened
-// socket.onopen = function (event) {
-//   console.log('Connected', event);
-//   socket.send('Hello Server!');
-// };
-
-// // Listen for messages
-// socket.onmessage = function (event) {
-//   console.log('Message from server ', event.data);
-// }
-
-// socket.onclose = function(event) {
-//   console.log("WebSocket is closed now.");
-// };
-
 const Socket = {
   socket: null,
   boot (ip) {
     ip = ip || 'localhost'
     console.log(`connnecting to ${ip}`)
     this.socket = new WebSocket(`ws://${ip}:8080`);
-    this.socket.onopen = function (event) {
+    this.socket.onopen = (event) => {
       console.log('Connected', event);
       this.socket.send('Hello Server!');
     };
     
     // Listen for messages
-    this.socket.onmessage = function (event) {
+    this.socket.onmessage = (event) => {
       console.log('Message from server ', event.data);
     }
     
-    this.socket.onclose = function(event) {
+    this.socket.onclose = (event) => {
       console.log("WebSocket is closed now.");
     };
   }
