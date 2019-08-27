b0791df36affe0b909ba7c4af7a489e2a1d9639b
Adam-Paterson1 <adam.paterson03@gmail.com>
2019-08-27 07:47:03 +0000

    Updates

---
+++ precache-manifest.d4120ef32506aed6667a8ef6d5085fcb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5a5d3644b90c64057dbeb6f0b0815a2c",
+    "url": "/fyp_frontend/index.html"
+  },
+  {
+    "revision": "84ef669bd42d72355ab0",
+    "url": "/fyp_frontend/static/css/main.6df04527.chunk.css"
+  },
+  {
+    "revision": "4627c6f3beb3daaca656",
+    "url": "/fyp_frontend/static/js/2.2aee87ff.chunk.js"
+  },
+  {
+    "revision": "84ef669bd42d72355ab0",
+    "url": "/fyp_frontend/static/js/main.66773f01.chunk.js"
+  },
+  {
+    "revision": "3b0d5d20d342e133ebac",
+    "url": "/fyp_frontend/static/js/runtime~main.75acad4d.js"
+  }
+]);
